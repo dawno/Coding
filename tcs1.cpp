eeb0b40b450a509baf4693a3692bfa19bd7f811f
Dawno <utkarshsinghcs@gmail.com>
2018-12-15 16:44:10 +0530

    improved time complexity

---
+++ tcs1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str, nst;
+        cin>>str;
+        int c=1; // last wale ki tarah hai sab bs isme c= 1 se start kiya hai kyuki element ek baar to atleast hoga he ..we are not searching here..we are comparing
+        int l= str.length();
+        for(int i=0;i<l;i++){
+            if(str[i]==str[i+1]){
+                c=c+1;
+            }
+            else{
+                nst= nst+ to_string(c)+str[i];
+                c=1;
+            }
+        }
+        cout<<nst<<endl;
+    }
+
+}
