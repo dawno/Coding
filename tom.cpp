9a5e0b260e20dd8e791083ef18ea0cca2702466b
Dawno <utkarshsinghcs@gmail.com>
2019-01-06 20:26:19 +0530

    update tom tom

--- tom.cpp
+++ tom.cpp
@@ -7,7 +7,7 @@
 double solution(int x1, int y1, int r1, int x2, int y2, int r2) {
     double dsq= (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
     double radSumSq = (r1 + r2) * (r1 + r2);
-    double d= sqrt(distsq);
+    double d= sqrt(dsq);
     double r1sq= pow(r1,2);
     double r2sq= pow(r2,2);
     double d1= (r1sq-r2sq+dsq)/(2*d);
@@ -16,13 +16,15 @@ double solution(int x1, int y1, int r1, int x2, int y2, int r2) {
     double d2sq= pow(d2,2);
     double X=  sqrt(r1sq-d1sq);
     double Y = sqrt(r2sq-d2sq);
-    if(dsq<radSumsq){
-        double A= r1sq* acos(d1/r1);
-        double B = d1*sqrt(X);
-        double C= r2sq * acos(d2/r2);
-        double D =d2* sqrt(Y);
+    double angle1 = d1/r1;
+    double angle2= d2/r2;
+    if(dsq<radSumSq){
+        double A= r1sq* acos(angle1);
+        double B = d1*X;
+        double C= r2sq * acos(angle2);
+        double D =d2* Y;
         double result = A-B+C-D;
-        return D;
+        return result;
     }
         else {
             return 0.0;
